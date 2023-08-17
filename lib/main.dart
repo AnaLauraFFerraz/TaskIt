7c383e593df5db09b2d5db2a5fe707483d8b875a
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-08-17 18:31:00 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TodoListPage(),
+    );
+  }
+}
+
+class TodoListPage extends StatelessWidget {
+  const TodoListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.lightBlue,
+    );
+  }
+}
